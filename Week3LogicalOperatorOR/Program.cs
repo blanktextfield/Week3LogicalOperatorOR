381e592ac0f8ded5ea39085e1d2db4af194f9a7d
Opilane TTHK <opilane@tthk.ee>
2020-09-15 14:11:55 +0300

    Week3LogicalOperatorOR

---
+++ Week3LogicalOperatorOR/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Week3LogicalOperatorOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what month were you born in?");
+            string month = Console.ReadLine().ToLower();
+            if (month == "december" || month == "january" || month == "febuary")
+
+            {
+                Console.WriteLine("You were born in winter");
+            } 
+            else if (month == "march" || month == "april" || month == "may")
+            {
+                Console.WriteLine("You were born in spring");
+
+            }
+            else if (month == "june" || month == "july" || month == "august")
+            {
+                Console.WriteLine("You were born in summer");
+            } 
+            else if (month == "september" || month == "october" || month == "november")
+            {
+                Console.WriteLine("You were  born in autumn");
+            } else
+            {
+                Console.WriteLine("THIS NOT A MONTH A FUCKING MOTNH YOU DONKEY!");
+            }
+
+
+
+        }  
+        
+         
+
+
+        
+    }
+}
